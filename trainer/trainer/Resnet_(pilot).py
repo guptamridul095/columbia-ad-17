6649aae652fee4ac4fd2f69626d77402ed444ed5
guptamridul095 <44420573+guptamridul095@users.noreply.github.com>
2019-08-14 17:34:35 -0400

    Update Resnet_(pilot).py

--- trainer/trainer/Resnet_(pilot).py
+++ trainer/trainer/Resnet_(pilot).py
@@ -141,6 +141,6 @@ def _cnn_model_fn(input_shape=(256, 256, 256, 1), classes=6):
     model = Model(inputs = X_input, outputs = X, name='ResNet50')
 
     return model
-model = ResNet50(input_shape = (64, 64, 3), classes = 6)
+model = _cnn_model_fn(input_shape = (64, 64, 3), classes = 6)
 model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
